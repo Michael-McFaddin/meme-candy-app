e8f69b619c0350b08b40d65c6600e3668756f976
“dziekan30” <dziekan30@gmail.com>
2020-05-04 16:24:35 -0500

    working app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 function Footer() {
   return (
     <Card>
-      <Card.Body style={{backgroundColor: "#894B94", bottom: 0, height: 300}}>
+      <Card.Body style={{backgroundColor: "#894B94", bottom: 0, height: 400}}>
         <Card.Title className="display-1" style={{ fontWeight: "bold", textAlign: "center", margin: 50  }}>Thank You For Tasting Our Delicious Meme Candy! </Card.Title>
       </Card.Body>
     </Card>
